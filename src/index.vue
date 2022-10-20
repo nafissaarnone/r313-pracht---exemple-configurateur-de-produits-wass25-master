293500c418e85207ca10111819d1b8a3a94d9bdf
wass25 <wassel.azzouz@edu.univ-fcomte.fr>
2022-10-20 20:08:41 +0200

    Liste d'exemples

---
+++ src/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+import exemples from "../public/exemples.json";
+import BasketProfil from "../src/components/BasketProfil.vue";
+import BasketDessus from "../src/components/BasketDessus.vue"
+
+
+
+</script>
+
+
+
+
+<template>
+
+    <div class="flex gap-5 border-0">
+
+        <div v-for="exemple in exemples" :key="exemple.semelle">
+
+            <BasketProfil class="carousel-item w-64" v-bind="exemple" id="profil" />
+            <BasketDessus class="carousel-item w-64" v-bind="exemple" id="dessus" />
+
+        </div>
+
+    </div>
+
+</template>
