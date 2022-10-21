89e5fba0fa4977f80bda56bf237dc92227eb39c2
wass25 <wassel.azzouz@edu.univ-fcomte.fr>
2022-10-21 10:26:34 +0200

    couleurs par liste de bouton "radio"

--- src/components/FormBasket.vue
+++ src/components/FormBasket.vue
@@ -39,14 +39,14 @@ if (props.id) {
         </div>
 
         <FormKit type="form" v-model="Baskete">
-            <FormKit name="semelle" label="semelle" value="#FFFFFF" type="select" :options="colors" />
-            <FormKit name="empeigne" label="empeigne" value="#FFFFFF" type="select" :options="colors" />
-            <FormKit name="pointe" label="pointe" value="#FFFFFF" type="select" :options="colors" />
-            <FormKit name="oeillet" label="oeillet" value="#FFFFFF" type="select" :options="colors" />
-            <FormKit name="bande" label="bande" value="#FFFFFF" type="select" :options="colors" />
-            <FormKit name="languette" label="languette" value="#FFFFFF" type="select" :options="colors" />
-            <FormKit name="lacet" label="lacet" value="#FFFFFF" type="select" :options="colors" />
-            <FormKit name="trimestre" label="trimestre" value="#FFFFFF" type="select" :options="colors" />
+            <FormKit name="semelle" label="semelle" value="#FFFFFF" type="radio" :options="colors" />
+            <FormKit name="empeigne" label="empeigne" value="#FFFFFF" type="radio" :options="colors" />
+            <FormKit name="pointe" label="pointe" value="#FFFFFF" type="radio" :options="colors" />
+            <FormKit name="oeillet" label="oeillet" value="#FFFFFF" type="radio" :options="colors" />
+            <FormKit name="bande" label="bande" value="#FFFFFF" type="radio" :options="colors" />
+            <FormKit name="languette" label="languette" value="#FFFFFF" type="radio" :options="colors" />
+            <FormKit name="lacet" label="lacet" value="#FFFFFF" type="radio" :options="colors" />
+            <FormKit name="trimestre" label="trimestre" value="#FFFFFF" type="radio" :options="colors" />
 
         </FormKit>
     </div>
