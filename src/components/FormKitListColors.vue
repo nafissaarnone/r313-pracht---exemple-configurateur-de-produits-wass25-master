9e15909d00c2a343426eb91315302f70d024a4a5
wass25 <wassel.azzouz@edu.univ-fcomte.fr>
2022-10-21 10:58:09 +0200

    Mise en forme du champ personnalisé.

--- src/components/FormKitListColors.vue
+++ src/components/FormKitListColors.vue
@@ -7,11 +7,14 @@ defineProps<{
 
 </script>
 <template>
-    <FormKit :name="name" :label="label" value="#FFFFFF" type="radio" :options="colors">
-
+    <FormKit :name="name" :label="label" value="#FFFFFF" type="radio" :options="colors" :sections-schema="{
+        inner: {$el: null},
+        decorator: {$el: null},
+    }" input-class="peer sr-only" options-class="flex gap-1">
         <template #label="context">
-            <div class="h-6 w-6 rounded-full" :style="{backgroundColor: context.option.value}"></div>
-            <span>{{ context.option.label}}</span>
+            <div class="h-6 w-6 rounded-full border-2 peer-checked:border-red-600"
+                :style="{backgroundColor: context.option.value}"></div>
+            <span class="sr-only">{{ context.option.label}}</span>
         </template>
     </FormKit>
 </template>
